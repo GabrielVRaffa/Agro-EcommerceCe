b0c6ed52eb16b8892a947282043e7924abeff556
Gabriel Vinícius Raffaelly <gabrielraffaelly@outlook.com>
2025-06-05 20:32:18 -0300

    Adição do load das paginas e melhorias na nevbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Login from './pages/Login';
@@ -9,25 +9,46 @@ import Orders from './pages/Orders';
 import ShoppingCart from './pages/ShoppingCart';
 import Navbar from './components/Navbar';
 import Layout from './pages/Layout';
-import FecharPedido from './pages/FecharPedido'
+import FecharPedido from './pages/FecharPedido';
 import { AuthProvider } from './components/AuthContext';
 import { CartProvider } from './components/CartContext';
+import Loader from './components/Loader';
+
+function AppContent() {
+  const location = useLocation();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => setLoading(false), 400); // ajustável
+    return () => clearTimeout(timer);
+  }, [location]);
+
+  return (
+    <>
+      <Navbar />
+      {loading && <Loader />}
+      {!loading && (
+        <Routes>
+          <Route path="/" element={<Layout><Home /></Layout>} />
+          <Route path="/products" element={<Layout><Products /></Layout>} />
+          <Route path="/login" element={<Layout><Login /></Layout>} />
+          <Route path="/cadastro" element={<Layout><SignUp /></Layout>} />
+          <Route path="/pedidos" element={<Layout><Orders /></Layout>} />
+          <Route path="/carrinho" element={<Layout><ShoppingCart /></Layout>} />
+          <Route path="/fechar-pedido" element={<Layout><FecharPedido /></Layout>} />
+        </Routes>
+      )}
+    </>
+  );
+}
 
 export default function App() {
   return (
     <AuthProvider>
       <CartProvider>
         <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Layout><Home /></Layout>} />
-            <Route path="/products" element={<Layout><Products /></Layout>} />
-            <Route path="/login" element={<Layout><Login /></Layout>} />
-            <Route path="/cadastro" element={<Layout><SignUp /></Layout>} />
-            <Route path="/pedidos" element={<Layout><Orders /></Layout>} />
-            <Route path="/carrinho" element={<Layout><ShoppingCart /></Layout>} />
-            <Route path="/fechar-pedido" element ={<Layout><FecharPedido /></Layout>} />
-          </Routes>
+          <AppContent />
         </Router>
       </CartProvider>
     </AuthProvider>
